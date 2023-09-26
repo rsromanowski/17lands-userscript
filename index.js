6ab7c5a1d7bb6428c16173125daf1ffe34fc42ba
Richard Romanowski <8516475+rsromanowski@users.noreply.github.com>
2023-09-26 15:10:52 -0400

    ignore advanced syntax

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.17lands.com/card_data
 // @grant       none
-// @version     1.1.3
+// @version     1.1.4
 // @author      rsromanowski
 // @license     MIT
 // @description Adds a input to quickly filter cards by name. Separate by commas to see multiple cards. Click `/` to quickly focus on input
@@ -12,10 +12,10 @@
 // ==/UserScript==
 
 function prettify() {
-  const toolbar = document.querySelector('div>div>select').parentElement.parentElement;
+  const toolbar = document.querySelector('div > div > select').parentElement.parentElement;
   toolbar.style.cssText += " justify-content: space-between"
 
-  const divs = document.querySelectorAll('.container-xxl>div')
+  const divs = document.querySelectorAll('.container-xxl > div')
   divs.forEach(d => d.classList.add('container'))
 }
 
@@ -24,17 +24,23 @@ function filterTable(filter) {
     { name: 'Name', index: 0, isFilter: true },
     { name: 'Color', index: 1, isFilter: false },
     { name: 'Rarity', index: 2, isFilter: false },
-  ]
+  ];
+
   const filterColumns = columns.filter(c => c.isFilter).map(c => c.index)
 
-  const rarityRegex = new RegExp('r[:=][curm]','i')
-  const colorRegex = new RegExp('c[:=][wubrgcm]','i')
+  const rarityRegex = new RegExp('r[:=]([curm])','i')
+  const colorRegex = new RegExp('c[:=]([wubrgcm])','i')
 
   // Currently only one table on page
   // Headers are in thead
   const rows = document.querySelectorAll(`table > tbody > tr`)
 
-  const orFilter = filter.split(",").filter(f => f.trim().length > 0).map(f => f.trim()).join("|")
+  const clauses = filter.split(",")
+  const rarityClauses = clauses.filter(c => rarityRegex.test(c))
+  const colorClauses = clauses.filter(c => colorRegex.test(c))
+  const nameClauses = clauses.filter(c => !rarityRegex.test(c) && !colorRegex.test(c))
+
+  const orFilter = nameClauses.filter(f => f.trim().length > 0).map(f => f.trim()).join("|")
   const regex = new RegExp(`${orFilter}`, 'i')
   const isFoundInTds = td => regex.test(td.innerHTML)
   const isFound = childrenArr => childrenArr.some(isFoundInTds)
@@ -50,20 +56,17 @@ function filterTable(filter) {
 function createQueryInput() {
   const div = document.querySelector('.main-text');
   const i = document.createElement('input');
-  i.id = "q"
-  i.className = "form-control"
-  i.placeholder = 'Press \'/\' to focus. Search by partial card names. i.e. "Candy Grapple" or "candy,torch"'
+  i.id = "q";
+  i.className = "form-control";
+  i.placeholder = 'Press \'/\' to focus. Search by partial card names. i.e. "Candy Grapple" or "candy,torch"';
   div.appendChild(i);
 
-  i.addEventListener("input", (event) => {
-    console.log(`${event.target.value}`);
-    filterTable(event.target.value);
-  });
+  i.addEventListener("input", (event) => { filterTable(event.target.value); });
 
   i.addEventListener("focus", function() { this.select(); });
 }
 
-const disconnect = VM.observe(document.body, () => {
+VM.observe(document.body, () => {
   const table = document.querySelector('table');
 
   if (table) {
