1d7dd676a658a0456006928991abab3110ad4af3
Richard Romanowski <richard@romanow.ski>
2023-09-30 09:45:54 -0400

    add lib file

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # 17Lands Userscript Filter
 
 ## Usage
+### Card Data
 1. Press `/` to focus on input
 2. Type partial card names to filter table. Examples: `Candy Grapple` or `torch`
 3. Separate by comma to see multiple cards. Example: `candy,torch,cooped`
+
+### Card Details
+As of October 2023, this feature requires Common patron tier or above
+1. Press
+
 
 ## Deploy
 1. Sync manually via https://greasyfork.org/en/scripts/476113-filter-17lands-data/admin after push/merge to main
--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.17lands.com/*
 // @grant       none
-// @version     1.3.0
+// @version     1.3.1
 // @author      rsromanowski
 // @license     MIT
 // @description Adds a input to quickly filter cards by name. Separate by commas to see multiple cards. Click `/` to quickly focus on input
 // @require https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @require https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
+// @require lib.js
 // ==/UserScript==
 
 function prettify() {
