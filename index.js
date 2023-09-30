456317e22e97977419de4cd71dddb283b6b3a169
Richard Romanowski <richard@romanow.ski>
2023-09-30 10:02:45 -0400

    move to switch

--- index.js
+++ index.js
@@ -11,63 +11,82 @@
 // @require https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
 // ==/UserScript==
 
-function prettify() {
-  const toolbar = document.querySelector('div > div > select').parentElement.parentElement
-  toolbar.style.cssText += " justify-content: space-between"
-
-  const divs = document.querySelectorAll('#app > div > div')
-  divs.forEach(d => d.classList.add('container'))
+switch(window.location.pathname) {
+  case '/card_data':
+    initCardData()
+    break
+  case '/card_data/details':
+    initCardDetails()
+    break
+  default: 
+    console.log(`Nothing to do on page: ${window.location.pathname}`)
+    break
+}
+if (window.location.pathname == '/card_data') {
+  initCardData()
+} else if (window.location.pathname == '/card_data/details') {
+  initCardDetails()
+} else {
+  console.log(`Nothing to do on page: ${window.location.pathname}`)
 }
 
-function filterTable(filter) {
-  const columns = [
-    { name: 'Name', index: 0, isFilter: true },
-    { name: 'Color', index: 1, isFilter: false },
-    { name: 'Rarity', index: 2, isFilter: false },
-  ]
-
-  const filterColumns = columns.filter(c => c.isFilter).map(c => c.index)
-
-  const rarityRegex = new RegExp('r[:=]([curm])', 'i')
-  const colorRegex = new RegExp('c[:=]([wubrgcm])', 'i')
-
-  // Currently only one table on page
-  // Headers are in thead
-  const rows = document.querySelectorAll(`table > tbody > tr`)
-
-  const clauses = filter.split(",")
-  const rarityClauses = clauses.filter(c => rarityRegex.test(c))
-  const colorClauses = clauses.filter(c => colorRegex.test(c))
-  const nameClauses = clauses.filter(c => !rarityRegex.test(c) && !colorRegex.test(c))
-
-  const orFilter = nameClauses.filter(f => f.trim().length > 0).map(f => f.trim()).join("|")
-  const regex = new RegExp(`${orFilter}`, 'i')
-  const isFoundInTds = td => regex.test(td.innerHTML)
-  const isFound = childrenArr => childrenArr.some(isFoundInTds)
-  const setTrStyleDisplay = ({ style, children }) => {
-    style.display = isFound([
-      ...filterColumns.map(c => children[c]) // <-- filter Columns
-    ]) ? '' : 'none'
+function initCardData() {
+  function prettify() {
+    const toolbar = document.querySelector('div > div > select').parentElement.parentElement
+    toolbar.style.cssText += " justify-content: space-between"
+  
+    const divs = document.querySelectorAll('#app > div > div')
+    divs.forEach(d => d.classList.add('container'))
+  }
+  
+  function filterTable(filter) {
+    const columns = [
+      { name: 'Name', index: 0, isFilter: true },
+      { name: 'Color', index: 1, isFilter: false },
+      { name: 'Rarity', index: 2, isFilter: false },
+    ]
+  
+    const filterColumns = columns.filter(c => c.isFilter).map(c => c.index)
+  
+    const rarityRegex = new RegExp('r[:=]([curm])', 'i')
+    const colorRegex = new RegExp('c[:=]([wubrgcm])', 'i')
+  
+    // Currently only one table on page
+    // Headers are in thead
+    const rows = document.querySelectorAll(`table > tbody > tr`)
+  
+    const clauses = filter.split(",")
+    const rarityClauses = clauses.filter(c => rarityRegex.test(c))
+    const colorClauses = clauses.filter(c => colorRegex.test(c))
+    const nameClauses = clauses.filter(c => !rarityRegex.test(c) && !colorRegex.test(c))
+  
+    const orFilter = nameClauses.filter(f => f.trim().length > 0).map(f => f.trim()).join("|")
+    const regex = new RegExp(`${orFilter}`, 'i')
+    const isFoundInTds = td => regex.test(td.innerHTML)
+    const isFound = childrenArr => childrenArr.some(isFoundInTds)
+    const setTrStyleDisplay = ({ style, children }) => {
+      style.display = isFound([
+        ...filterColumns.map(c => children[c]) // <-- filter Columns
+      ]) ? '' : 'none'
+    }
+  
+    rows.forEach(setTrStyleDisplay)
+  }
+  
+  function createQueryInput() {
+    const div = document.querySelector('.main-text')
+    const i = document.createElement('input')
+    i.id = "q"
+    i.className = "form-control"
+    i.placeholder = 'Press \'/\' to focus. Search by partial card names. i.e. "Candy Grapple" or "candy,torch"'
+    div.appendChild(i)
+  
+    i.addEventListener("input", (event) => { filterTable(event.target.value) })
+  
+    // Select all text of focus for quick overwrite
+    i.addEventListener("focus", function () { this.select() })
   }
 
-  rows.forEach(setTrStyleDisplay)
-}
-
-function createQueryInput() {
-  const div = document.querySelector('.main-text')
-  const i = document.createElement('input')
-  i.id = "q"
-  i.className = "form-control"
-  i.placeholder = 'Press \'/\' to focus. Search by partial card names. i.e. "Candy Grapple" or "candy,torch"'
-  div.appendChild(i)
-
-  i.addEventListener("input", (event) => { filterTable(event.target.value) })
-
-  // Select all text of focus for quick overwrite
-  i.addEventListener("focus", function () { this.select() })
-}
-
-if (window.location.pathname == '/card_data') {
   VM.observe(document.body, () => {
     const table = document.querySelector('table')
 
@@ -82,10 +101,12 @@ if (window.location.pathname == '/card_data') {
   VM.shortcut.register('/', () => {
     document.getElementById("q").focus()
   })
-} else if (window.location.pathname == '/card_data/details') {
-  const dropdown = document.getElementById('card')
+}
 
+function initCardDetails() {
   console.log(`details baby`)
+  
+  const dropdown = document.getElementById('card')
 
   VM.shortcut.register('k', () => {
     dropdown.selectedIndex = Math.max(0, dropdown.selectedIndex--)
@@ -95,6 +116,4 @@ if (window.location.pathname == '/card_data') {
     dropdown.selectedIndex = Math.min(dropdown.childElementCount - 1, dropdown.selectedIndex++)
     dropdown.dispatchEvent(new window.Event('change', { bubbles: true }))
   })
-} else {
-  console.log(`Nothing to do on page: ${window.location.pathname}`)
 }
